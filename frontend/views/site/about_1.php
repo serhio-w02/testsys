95bbbef7404dd50d79e1fcd0ddc0c0fa0f75b1af
serhio-w02 <serhio.w02@gmail.com>
2020-03-11 23:08:01 +0100

    first commit (2)

---
+++ frontend/views/site/about_1.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About-1';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Tole je About - 1111:</p>
+
+    <code><?= __FILE__ ?></code>
+</div>
